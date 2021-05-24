9502eed198f6d4ec0cf9a06db421fba8b0445c31
aileen <44736622+wua3@users.noreply.github.com>
2021-05-24 12:00:49 -0400

    Increased logo size + slowed passive mouse particle

--- src/particle_effect.js
+++ src/particle_effect.js
@@ -29,7 +29,7 @@ tsParticles.load("logo-stars", {
       blink: false,
       color: "#fff",
       consent: false,
-      distance: 14,
+      distance: 15,
       enable: true,
       opacity: 1,
       width: 1
@@ -53,10 +53,10 @@ tsParticles.load("logo-stars", {
     number: {
       density: {
         enable: false,
-        area: 10000
+        area: 20000
       },
       limit: 0,
-      value: 150
+      value: 175
     },
     opacity: {
       animation: {
@@ -86,13 +86,13 @@ tsParticles.load("logo-stars", {
     draw: {
       enable: true,
       lineColor: "#fff",
-      lineWidth: 0.24
+      lineWidth: 0.20
     },
     move: {
       radius: 3
     },
     inlineArrangement: "equidistant",
-    scale: 0.48,
+    scale: 0.6,
     type: "inline",
     data: {
       path: "M1259.83 782.15h-18.44a7.3 7.3 0 00-6.38 4.44l-77.7 209.65-78-209.64a7.32 7.32 0 00-6.39-4.44h-18.44a3.07 3.07 0 00-3.09 4.44l91.19 245.18a6 6 0 00.93 1.6l-9.83 26.53-101.27-273.3a7.3 7.3 0 00-6.38-4.44h-18.44a3.08 3.08 0 00-3.09 4.44l114.16 308a6.59 6.59 0 001.92 2.63 3.1 3.1 0 003.24 2.13h18.44a6.13 6.13 0 002.11-.41 3.18 3.18 0 001.64-.85 6.91 6.91 0 002.63-3.19l114.28-308.32a3.08 3.08 0 00-3.09-4.45zM1354.93 782.15h-18.44a7.3 7.3 0 00-6.38 4.44L1215.83 1095a3.08 3.08 0 003.09 4.44h18.44a7.31 7.31 0 006.38-4.44L1358 786.6a3.08 3.08 0 00-3.07-4.45zM1240.89 960.6h-18.44a7.3 7.3 0 00-6.38 4.44L1167.92 1095a3.07 3.07 0 003.06 4.44h18.51a7.31 7.31 0 006.3-4.33L1244 965a3.07 3.07 0 00-3.11-4.4zM1229.63 940.83h18.44a7.3 7.3 0 006.38-4.44L1310 786.48c.77-2.07-.11-3.8-2-4.29a3.89 3.89 0 00-.42 0H1288a5.22 5.22 0 00-1.5.25 7.78 7.78 0 00-3.3 2.28 5.24 5.24 0 00-.78 1.37l-56.1 151.37c-.25 1.91 1.07 3.37 3.31 3.37z",
@@ -217,7 +217,7 @@ tsParticles.load("background-stars", {
         particles_nb: 1
       },
       trail: {
-        delay: .6,
+        delay: 1.5,
         quantity: 1
       },
       remove: {
