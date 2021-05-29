d2d92a13d85f8674fcdec18b4ebebd16fb9e9d82
aileen <44736622+wua3@users.noreply.github.com>
2021-05-29 10:33:59 -0400

    Run prettier

--- src/form.js
+++ src/form.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAMg5ALVWNDSLux3yI-P9ftZYXCt4_4ViM",
-    authDomain: "vh7-preregistration.firebaseapp.com",
-    databaseURL: "https://vh7-preregistration.firebaseio.com",
-    projectId: "vh7-preregistration",
-    storageBucket: "vh7-preregistration.appspot.com",
-    messagingSenderId: "70446224469",
-    appId: "1:70446224469:web:a66c6be57c125245d8f0dc"
+  apiKey: 'AIzaSyAMg5ALVWNDSLux3yI-P9ftZYXCt4_4ViM',
+  authDomain: 'vh7-preregistration.firebaseapp.com',
+  databaseURL: 'https://vh7-preregistration.firebaseio.com',
+  projectId: 'vh7-preregistration',
+  storageBucket: 'vh7-preregistration.appspot.com',
+  messagingSenderId: '70446224469',
+  appId: '1:70446224469:web:a66c6be57c125245d8f0dc'
 };
 // // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -15,31 +15,30 @@ firebase.initializeApp(firebaseConfig);
 var emailRef = firebase.database().ref('emails');
 
 // Listen for form submit
-var emailField = document.getElementById('email')
+var emailField = document.getElementById('email');
 emailField.addEventListener('keyup', e => {
-    if (e.keyCode === 13) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
 
-        e.preventDefault();
-
-        var label = document.getElementById('form__label');
-        // Validate email address
-        if (emailField.validity.typeMismatch) {
-            label.innerHTML = "please enter a valid email address";
-        } else {
-            // Send to firebase
-            saveEmail();
-            // Feedback to user that submission was successful
-            emailField.value = '';
-            label.innerHTML = "thank you for subscribing!";
-        }
+    var label = document.getElementById('form__label');
+    // Validate email address
+    if (emailField.validity.typeMismatch) {
+      label.innerHTML = 'please enter a valid email address';
+    } else {
+      // Send to firebase
+      saveEmail();
+      // Feedback to user that submission was successful
+      emailField.value = '';
+      label.innerHTML = 'thank you for subscribing!';
     }
+  }
 });
 
 // Save message to firebase
 function saveEmail() {
-    var email = document.getElementById('email').value;
-    var newEmailRef = emailRef.push();
-    newEmailRef.set({
-        email: email
-    });
+  var email = document.getElementById('email').value;
+  var newEmailRef = emailRef.push();
+  newEmailRef.set({
+    email: email
+  });
 }
